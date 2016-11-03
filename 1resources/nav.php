d37471487a4cd03519b84a4d2a80701d7e5d5c80
Jackson Knorr <jackson.knorr@my.simpson.edu>
2016-11-03 12:24:17 -0500

    working

--- 1resources/nav.php
+++ 1resources/nav.php
@@ -1,10 +1,10 @@
 <nav>
       <ul>
-        <li> <a href="index.html">Main Page</a> </li>
-        <li> <a href="football.html">Football</a> </li>
-        <li> <a href="basketball.html">Basketball</a> </li>         
-        <li> <a href="baseball.html">Baseball</a> </li>          
-        <li> <a href="articles.html">Articles of the Day</a> </li>         
-        <li> <a href="about.html">About</a> </li>
+        <li> <a href="index.php">Main Page</a> </li>
+        <li> <a href="football.php">Football</a> </li>
+        <li> <a href="basketball.php">Basketball</a> </li>         
+        <li> <a href="baseball.php">Baseball</a> </li>          
+        <li> <a href="articles.php">Articles of the Day</a> </li>         
+        <li> <a href="about.php">About</a> </li>
       </ul>
     </nav>
